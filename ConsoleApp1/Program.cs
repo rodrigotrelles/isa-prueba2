f238e19e3cefc9c62406aff9971ccdc16ade0f1b
Rodrigo Trelles <rodrigo.trelles5@gmail.com>
2023-04-17 22:14:36 -0300

    fix Progarm.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Mai(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
